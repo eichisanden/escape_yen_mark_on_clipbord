4fc4e19e1c0d948506024337e04ed7972ee436af
eichisanden <eichisanden@gmail.com>
2016-03-09 00:52:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# escape_yen_mark_on_clipbord
+
+Escape Yen mark(`¥`) on clipboard.
+
+## Usage
+
+Execute `escape_yen_mark_on_clipbord`
+
+Before
+```
+c:¥windows¥system32¥drivers¥etc
+```
+
+After
+```
+c:¥¥windows¥¥system32¥¥drivers¥¥etc
+```
---
+++ escape_yen_mark_on_clipbord.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/atotto/clipboard"
+	"strings"
+)
+
+func main() {
+	text, err := clipboard.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	text = strings.Replace(text, "¥", "¥¥", -1)
+	clipboard.WriteAll(text)
+}
